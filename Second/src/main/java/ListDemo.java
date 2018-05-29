214ed4f3f94770c0fd882d8cb15e1d0357389f22
Lilya Shuenbekova <31605257+lilyash@users.noreply.github.com>
2018-05-29 15:10:15 +0600

    Add files via upload

--- Second/src/main/java/ListDemo.java
+++ Second/src/main/java/ListDemo.java
@@ -1,10 +1,11 @@
 import java.util.*;
 
 public class ListDemo {
+    // Вход: список строк и символ. Выход: количество строк входного списка, у которых первый символ совпадает с заданным.
         public static int getNumberOfCharInString(List<String> listOfString, char first) {
             int res = 0;
             for (int i = 0; i < listOfString.size(); i++) {
-                if (!listOfString.get(i).equals("")) {
+                if (!listOfString.get(i).isEmpty()) {
                     if (first == listOfString.get(i).charAt(0)) {
                         res++;
                     }
@@ -12,7 +13,7 @@ public class ListDemo {
             }
             return res;
         }
-
+        // список однофамильцев
         public static List<Human> getListSameSurnameWithHenry(List<Human> listOfHumans, Human goodHenry) {
             List<Human> result = new ArrayList<>();
             int pos = 0;
@@ -24,8 +25,8 @@ public class ListDemo {
             }
             return result;
         }
-
-        public static List<Human> deletHenryFromList(List<Human> listOfHumans, Human poorHenry) {
+            // список без Генри
+        public static List<Human> deleteHenryFromList(List<Human> listOfHumans, Human poorHenry) {
             List<Human> result = new ArrayList<>();
             int pos = 0;
             for (int i = 0; i < listOfHumans.size(); i++) {
@@ -36,102 +37,34 @@ public class ListDemo {
             }
             return result;
         }
-
+        //Вход: список множеств целых чисел и еще одно множество. Выход: список всех множеств
+       // входного списка, которые не пересекаются с заданным множеством.
         public static List<Set<Integer>> removeIntersectSets(List<Set<Integer>> listOfSets, Set<Integer> set) {
             List<Set<Integer>> result = new ArrayList<>();
-            int pos = 0, size;
+//            int pos = 0, size;
             boolean flag = true;
-            Iterator<Integer> iter;
-            Set<Integer> copy = new HashSet<>();
-            for (int i = 0; i < listOfSets.size(); i++) {
-                iter = listOfSets.get(i).iterator();
-                while (iter.hasNext() && flag) {
-                    if (set.contains(iter.next())) {
-                        flag = false;
-                    }
+            //Iterator<Integer> iter;
+           // Set<Integer> copy = new HashSet<>();
+            for (Set<Integer> iter : listOfSets) {
+                if (Collections.disjoint(iter, set)) {
+                    result.add(iter);
                 }
-                if (flag) {
-                    result.add(pos, listOfSets.get(i));
-                    pos++;
-                }
-                flag = true;
+
+//                while (iter.hasNext() && flag) {
+//                    if (set.contains(iter.next())) {
+//                        flag = false;
+//                    }
+//                }
+//                if (flag) {
+//                   result.add(pos, listOfSets.get(i));
+//                    pos++;
+//                }
+//                flag = true;
             }
             return result;
         }
 
-        public static Set<Human> getSetOfOldest(List<Human> listOfHumans) {
-            Set<Human> result = new HashSet<>();
-            int maxAge = 0, pos = 0;
-            for (int i = 0; i < listOfHumans.size(); i++) {
-                if (maxAge < listOfHumans.get(i).getAge()) {
-                    result.clear();
-                    maxAge = listOfHumans.get(i).getAge();
-                    result.add(listOfHumans.get(i));
-                }
-                if (maxAge == listOfHumans.get(i).getAge()) {
-                    result.add(listOfHumans.get(i));
-                }
-            }
-            return result;
-        }
-
-        public static List<Human> makeListFromSet(Set<Human> setOfHumans) {
-            List<Human> result = new ArrayList<>();
-            int pos = 0;
-            int numberOfElem = 0;
-            boolean flag = true;
-            Iterator<Human> iter = setOfHumans.iterator();
-            Human someJohn;
-            while (iter.hasNext()) {
-                if (result.isEmpty()) {
-                    result.add(0, iter.next());
-                    numberOfElem++;
-                } else {
-                    someJohn = iter.next();
-                    while (flag) {
-
-                        if (result.get(pos).getSurname().compareTo(someJohn.getSurname()) > 0) {
-                            result.add(pos, someJohn);
-                            flag = false;
-                            numberOfElem++;
-                        } else {
-                            if ((result.get(pos).getSurname().compareTo(someJohn.getSurname()) == 0) &&
-                                    result.get(pos).getName().compareTo(someJohn.getName()) > 0) {
-                                result.add(pos, someJohn);
-                                flag = false;
-                                numberOfElem++;
-                            } else {
-                                if (result.get(pos).getSurname().compareTo(someJohn.getSurname()) == 0 &&
-                                        result.get(pos).getName().compareTo(someJohn.getName()) == 0 &&
-                                        result.get(pos).getPatronymic().compareTo(someJohn.getPatronymic()) > 0) {
-                                    result.add(pos, someJohn);
-                                    flag = false;
-                                    numberOfElem++;
-                                } else {
-                                    if (result.get(pos).getSurname().compareTo(someJohn.getSurname()) == 0 &&
-                                            result.get(pos).getName().compareTo(someJohn.getName()) == 0 &&
-                                            result.get(pos).getPatronymic().compareTo(someJohn.getPatronymic()) == 0) {
-                                        result.add(pos, someJohn);
-                                        flag = false;
-                                        numberOfElem++;
-                                    }
-                                }
-                            }
-                        }
-                        pos++;
-                        if (pos == numberOfElem) {
-                            result.add(pos, someJohn);
-                            flag = false;
-                            numberOfElem++;
-                        }
-                    }
-                    flag = true;
-                    pos = 0;
-                }
-            }
-            return result;
-        }
-
+        // идентификатор
         public static Set<Human> identifeHumans(Map<Integer, Human> mapId, Set<Integer> setOfId) {
             Set<Human> result = new HashSet<>();
             Iterator<Integer> itr = setOfId.iterator();
@@ -144,22 +77,20 @@ public class ListDemo {
             }
             return result;
         }
-
+        // 18 мне уже
         public static Set<Integer> getIdAge(Map<Integer, Human> mapId) {
             Set<Integer> result = new HashSet<>();
-            Iterator<Human> iterValues = mapId.values().iterator();
+            //Iterator<Human> iterValues = mapId.values().iterator();
             Iterator<Integer> iterKeys = mapId.keySet().iterator();
-            Human bigJhonny;
+            Integer bigJhonnyKey;
             int id;
-            while (iterValues.hasNext()) {
-                bigJhonny = iterValues.next();
-                id = iterKeys.next();
-                if (bigJhonny.getAge() >= 18) {
-                    result.add(id);
+            while (iterKeys.hasNext()) {   // keys
+                bigJhonnyKey = iterKeys.next();  // mapId.get(id)
+                if (mapId.get(bigJhonnyKey).getAge() >= 18) {
+                    result.add(bigJhonnyKey);
                 }
             }
             return result;
         }
     }
 
-}
